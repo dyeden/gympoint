451f7e0c53892c131b3d6c7bd758aa691d3dce7d
Dyeden Monteiro <dyedenm@gmail.com>
2019-10-16 13:35:03 -0300

    prettier configurado

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,9 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
+    'airbnb-base', 'prettier'
   ],
+  plugins:['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,6 +16,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "prettier/prettier":"error",
     'class-methods-use-this': "off",
     "no-param-reassign": "off",
     "camelcase": "off",
